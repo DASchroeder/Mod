3c4344d360cc7e28a20bd466d33514241cda03b5
Daniel A Schroeder <danielschroederjr@gmail.com>
2017-05-09 16:54:18 -0400

    Add project files.

---
+++ Mod/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mod.Startup))]
+namespace Mod
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
