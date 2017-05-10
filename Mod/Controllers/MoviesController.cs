3be805b44ce6f6d4ed133cf1e0719d3ed35a6c95
Daniel A Schroeder <danielschroederjr@gmail.com>
2017-05-10 08:51:23 -0400

    implemented some attribute routing

--- Mod/Controllers/MoviesController.cs
+++ Mod/Controllers/MoviesController.cs
@@ -9,21 +9,17 @@ namespace Mod.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() {Name = "Shrek"};
             return View(movie);
         }
 
-        public ActionResult Index(int? pageIndex, string sortBy)
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseYear(int year, int month)
         {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (string.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-
+            return Content(year + "/" + month);
         }
     }
 }
