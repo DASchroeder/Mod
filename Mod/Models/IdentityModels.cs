ff744e61c4727da9c521afbcbdd0f7fd561dfc77
Daniel A Schroeder <danielschroederjr@gmail.com>
2017-05-10 16:26:46 -0400

    building forms
    model binding, add-edit customer

--- Mod/Models/IdentityModels.cs
+++ Mod/Models/IdentityModels.cs
@@ -20,9 +20,11 @@ namespace Mod.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public DbSet<Customer> Customers { get; set; }
 
+        public DbSet<Customer> Customers { get; set; }
         public DbSet<Movie> Movies { get; set; }
+        public DbSet<MembershipType> MembershipTypes { get; set; }
+
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
---
+++ Mod/ViewModels/CustomerFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Mod.Models;
+
+namespace Mod.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
