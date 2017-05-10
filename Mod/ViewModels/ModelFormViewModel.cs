3db5b86b144b31d510fff52259cf88bcd2357fde
Daniel A Schroeder <danielschroederjr@gmail.com>
2017-05-10 17:06:47 -0400

    added views to Movies
    enabled add movie, edit movie

---
+++ Mod/ViewModels/ModelFormViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Mod.Models;
+
+namespace Mod.ViewModels
+{
+    public class MovieFormViewModel
+    {
+        public IEnumerable<Genre> Genres { get; set; }
+        public Movie Movie { get; set; }
+        public string Title
+        {
+            get
+            {
+                if (Movie != null && Movie.Id != 0)
+                    return "Edit Movie";
+
+                return "New Movie";
+            }
+        }
+    }
+}
