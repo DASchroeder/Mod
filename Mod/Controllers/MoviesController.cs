4b5abdf9e43568bdbc9d731b3e7ac44b52129ac1
Daniel A Schroeder <danielschroederjr@gmail.com>
2017-05-10 14:40:51 -0400

    added views for customer details and movie details

--- Mod/Controllers/MoviesController.cs
+++ Mod/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,31 +11,45 @@ namespace Mod.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
         }
 
-        private IEnumerable<Movie> GetMovies()
+        public ActionResult Details(int id)
         {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Shrek" },
-                new Movie { Id = 2, Name = "Wall-e" }
-            };
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+
         }
+
+
         // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie() {Name = "Shrek"};
-
-            var viewResult = new ViewResult();
+            var movie = new Movie() { Name = "Shrek!" };
             var customers = new List<Customer>
             {
-                new Customer() {Name = "Customer 1"},
-                new Customer() {Name = "Customer 2"}
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
 
             var viewModel = new RandomMovieViewModel
@@ -45,11 +60,5 @@ namespace Mod.Controllers
 
             return View(viewModel);
         }
-
-        //[Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
-        //public ActionResult ByReleaseYear(int year, int month)
-        //{
-        //    return Content(year + "/" + month);
-        //}
     }
 }
